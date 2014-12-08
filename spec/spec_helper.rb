dc79ad931902552b94db6f3ae5a95f2670e96203
Tony Ta <tonyta.tt@gmail.com>
2014-12-07 23:40:08 -0800

    add fail-matchers for test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,24 @@ require 'pry-byebug'
 require 'rspec/its'
 require 'active_model_serializers'
 require 'active_model_serializers_matchers'
+
+module RSpec
+  module Matchers
+    def fail
+      raise_error(RSpec::Expectations::ExpectationNotMetError)
+    end
+
+    def fail_with(message)
+      raise_error(RSpec::Expectations::ExpectationNotMetError, message)
+    end
+
+    def fail_matching(message)
+      if String === message
+        regexp = /#{Regexp.escape(message)}/
+      else
+        regexp = message
+      end
+      raise_error(RSpec::Expectations::ExpectationNotMetError, regexp)
+    end
+  end
+end
