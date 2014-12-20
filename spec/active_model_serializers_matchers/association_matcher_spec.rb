03799f8e709242b616f74eadddce6228e5406350
Tony Ta <tonyta.tt@gmail.com>
2014-12-19 17:00:27 -0800

    extracts out shared examples for has_one :foo and has_many :foos

--- spec/active_model_serializers_matchers/association_matcher_spec.rb
+++ spec/active_model_serializers_matchers/association_matcher_spec.rb
@@ -3,6 +3,50 @@ require 'spec_helper'
 class FooSerializer
 end
 
+shared_examples 'it has_one :foo' do
+  describe 'association expectation' do
+    it 'should pass a have_one :foo expectation' do
+      expectation = have_one(:foo)
+      expect(subject).to expectation
+      expect(expectation.description).to eq('have one :foo')
+    end
+
+    it 'should fail a have_one :fuu expectation' do
+      expect {
+        expect(subject).to have_one(:fuu)
+      }.to fail_with("expected #{ subject } to define a 'has_one :fuu' association")
+    end
+
+    it 'should fail a have_many :foo expectation' do
+      expect {
+        expect(subject).to have_many(:foo)
+      }.to fail_with("expected #{ subject } to define a 'has_many :foo' association")
+    end
+  end
+end
+
+shared_examples 'it has_many :foos' do
+  describe 'association expectation' do
+    it 'should pass a have_many :foos expectation' do
+      expectation = have_many(:foos)
+      expect(subject).to expectation
+      expect(expectation.description).to eq('have many :foos')
+    end
+
+    it 'should fail a have_many :fuus expectation' do
+      expect {
+        expect(subject).to have_many(:fuus)
+      }.to fail_with("expected #{ subject } to define a 'has_many :fuus' association")
+    end
+
+    it 'should fail a have_one :foos expectation' do
+      expect {
+        expect(subject).to have_one(:foos)
+      }.to fail_with("expected #{ subject } to define a 'has_one :foos' association")
+    end
+  end
+end
+
 describe ActiveModelSerializersMatchers::AssociationMatcher do
 
   include ActiveModelSerializersMatchers
@@ -12,25 +56,7 @@ describe ActiveModelSerializersMatchers::AssociationMatcher do
       Class.new(ActiveModel::Serializer) { has_one :foo }
     end
 
-    describe 'association expectation' do
-      it 'should pass a have_one :foo expectation' do
-        expectation = have_one(:foo)
-        expect(subject).to expectation
-        expect(expectation.description).to eq('have one :foo')
-      end
-
-      it 'should fail a have_one :fuu expectation' do
-        expect {
-          expect(subject).to have_one(:fuu)
-        }.to fail_with("expected #{ subject } to define a 'has_one :fuu' association")
-      end
-
-      it 'should fail a have_many :foo expectation' do
-        expect {
-          expect(subject).to have_many(:foo)
-        }.to fail_with("expected #{ subject } to define a 'has_many :foo' association")
-      end
-    end
+    it_behaves_like 'it has_one :foo'
 
     describe 'key expectation' do
       it 'should fail a have_one :foo as :bar expectation' do
@@ -54,25 +80,7 @@ describe ActiveModelSerializersMatchers::AssociationMatcher do
       Class.new(ActiveModel::Serializer) { has_one :foo, key: :bar }
     end
 
-    describe 'association expectation' do
-      it 'should pass a have_one :foo expectation' do
-        expectation = have_one(:foo)
-        expect(subject).to expectation
-        expect(expectation.description).to eq('have one :foo')
-      end
-
-      it 'should fail a have_one :fuu expectation' do
-        expect {
-          expect(subject).to have_one(:fuu)
-        }.to fail_with("expected #{ subject } to define a 'has_one :fuu' association")
-      end
-
-      it 'should fail a have_many :foo expectation' do
-        expect {
-          expect(subject).to have_many(:foo)
-        }.to fail_with("expected #{ subject } to define a 'has_many :foo' association")
-      end
-    end
+    it_behaves_like 'it has_one :foo'
 
     describe 'key expectation' do
       it 'should pass a have_one :foo as :bar expectation' do
@@ -102,25 +110,7 @@ describe ActiveModelSerializersMatchers::AssociationMatcher do
       Class.new(ActiveModel::Serializer) { has_many :foos }
     end
 
-    describe 'association expectation' do
-      it 'should pass a have_many :foos expectation' do
-        expectation = have_many(:foos)
-        expect(subject).to expectation
-        expect(expectation.description).to eq('have many :foos')
-      end
-
-      it 'should fail a have_many :fuus expectation' do
-        expect {
-          expect(subject).to have_many(:fuus)
-        }.to fail_with("expected #{ subject } to define a 'has_many :fuus' association")
-      end
-
-      it 'should fail a have_one :foos expectation' do
-        expect {
-          expect(subject).to have_one(:foos)
-        }.to fail_with("expected #{ subject } to define a 'has_one :foos' association")
-      end
-    end
+    it_behaves_like 'it has_many :foos'
 
     describe 'key expectation' do
       it 'should fail a have_many :foos as :bars expectation' do
@@ -144,25 +134,7 @@ describe ActiveModelSerializersMatchers::AssociationMatcher do
       Class.new(ActiveModel::Serializer) { has_many :foos, key: :bars }
     end
 
-    describe 'association expectation' do
-      it 'should pass a have_many :foos expectation' do
-        expectation = have_many(:foos)
-        expect(subject).to expectation
-        expect(expectation.description).to eq('have many :foos')
-      end
-
-      it 'should fail a have_many :fuus expectation' do
-        expect {
-          expect(subject).to have_many(:fuus)
-        }.to fail_with("expected #{ subject } to define a 'has_many :fuus' association")
-      end
-
-      it 'should fail a have_one :foos expectation' do
-        expect {
-          expect(subject).to have_one(:foos)
-        }.to fail_with("expected #{ subject } to define a 'has_one :foos' association")
-      end
-    end
+    it_behaves_like 'it has_many :foos'
 
     describe 'key expectation' do
       it 'should pass a have_many :foos as :bars expectation' do
