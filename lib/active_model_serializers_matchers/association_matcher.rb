771973a9108ade28b7676125a72d4d981f1b6c9a
Tony Ta <tonyta.tt@gmail.com>
2014-12-19 16:16:45 -0800

    adds key check fail matcher

--- lib/active_model_serializers_matchers/association_matcher.rb
+++ lib/active_model_serializers_matchers/association_matcher.rb
@@ -1,32 +1,35 @@
 module ActiveModelSerializersMatchers
   class AssociationMatcher
 
-    attr_reader :root, :type, :association_check
+    attr_reader :root, :type, :checks
     attr_reader :actual
 
     def initialize(root:, type:)
       @root = root
       @type = type
-      @association_check = AssociationCheck.new(self)
-    #   key_check         = KeyCheck.new(self)
-    #   serializer_check  = SerializerCheck.new(self)
+      @checks = [AssociationCheck.new(self, type)]
     end
 
     def matches?(actual)
       @actual = actual.is_a?(Class) ? actual : actual.class
-      association_check.passes?
+      checks.all?(&:pass?)
     end
 
     def description
-      association_check.description
+      checks.map(&:description).join(' ')
     end
 
     def failure_message
-      association_check.failure_message
+      checks.find(&:fail?).failure_message
     end
 
     def root_association
       associations[root]
+    end
+
+    def as(key)
+      checks << KeyCheck.new(self, key)
+      self
     end
 
     private
@@ -37,15 +40,20 @@ module ActiveModelSerializersMatchers
 
     class AssociationCheck
 
-      attr_reader :matcher
+      attr_reader :matcher, :type
 
-      def initialize(matcher)
+      def initialize(matcher, type)
         @matcher = matcher
+        @type = type
       end
 
-      def passes?
+      def pass?
         return false if matcher.root_association.nil?
         matcher.root_association.superclass == association_type
+      end
+
+      def fail?
+        !pass?
       end
 
       def description
@@ -53,13 +61,13 @@ module ActiveModelSerializersMatchers
       end
 
       def failure_message
-        "expected #{ matcher.actual } to define a '#{ matcher.type } #{ matcher.root.inspect }' association"
+        "expected #{ matcher.actual } to define a '#{ type } #{ matcher.root.inspect }' association"
       end
 
       private
 
       def association_type
-        case matcher.type
+        case type
         when :has_one
           ActiveModel::Serializer::Associations::HasOne
         when :has_many
@@ -70,12 +78,34 @@ module ActiveModelSerializersMatchers
       end
 
       def association_string
-        case matcher.type
+        case type
         when :has_one
           'have one'
         when :has_many
           'have many'
         end
+      end
+    end
+
+    class KeyCheck
+
+      attr_reader :matcher, :key
+
+      def initialize(matcher, key)
+        @matcher = matcher
+        @key = key
+      end
+
+      def pass?
+        false
+      end
+
+      def fail?
+        !pass?
+      end
+
+      def failure_message
+        "expected #{ matcher.actual } '#{ matcher.type } #{ matcher.root.inspect }' association to explicitly have key #{ key.inspect } but instead has none"
       end
     end
   end
--- spec/active_model_serializers_matchers/association_matcher_spec.rb
+++ spec/active_model_serializers_matchers/association_matcher_spec.rb
@@ -9,22 +9,32 @@ describe ActiveModelSerializersMatchers::AssociationMatcher do
       Class.new(ActiveModel::Serializer) { has_one :foo }
     end
 
-    it 'should pass a have_one :foo expectation' do
-      expectation = have_one(:foo)
-      expect(subject).to expectation
-      expect(expectation.description).to eq('have one :foo')
+    describe 'association expectation' do
+      it 'should pass a have_one :foo expectation' do
+        expectation = have_one(:foo)
+        expect(subject).to expectation
+        expect(expectation.description).to eq('have one :foo')
+      end
+
+      it 'should fail a have_one :fuu expectation' do
+        expect {
+          expect(subject).to have_one(:fuu)
+        }.to fail_with("expected #{ subject } to define a 'has_one :fuu' association")
+      end
+
+      it 'should fail a have_many :foo expectation' do
+        expect {
+          expect(subject).to have_many(:foo)
+        }.to fail_with("expected #{ subject } to define a 'has_many :foo' association")
+      end
     end
 
-    it 'should fail a have_one :fuu expectation' do
-      expect {
-        expect(subject).to have_one(:fuu)
-      }.to fail_with("expected #{ subject } to define a 'has_one :fuu' association")
-    end
-
-    it 'should fail a have_many :foo expectation' do
-      expect {
-        expect(subject).to have_many(:foo)
-      }.to fail_with("expected #{ subject } to define a 'has_many :foo' association")
+    describe 'key expectation' do
+      it 'should fail a have_one :foo as :bar expectation' do
+        expect {
+          expect(subject).to have_one(:foo).as(:bar)
+        }.to fail_with("expected #{ subject } 'has_one :foo' association to explicitly have key :bar but instead has none")
+      end
     end
   end
 
@@ -33,22 +43,32 @@ describe ActiveModelSerializersMatchers::AssociationMatcher do
       Class.new(ActiveModel::Serializer) { has_many :foos }
     end
 
-    it 'should pass a have_many :foos expectation' do
-      expectation = have_many(:foos)
-      expect(subject).to expectation
-      expect(expectation.description).to eq('have many :foos')
+    describe 'association expectation' do
+      it 'should pass a have_many :foos expectation' do
+        expectation = have_many(:foos)
+        expect(subject).to expectation
+        expect(expectation.description).to eq('have many :foos')
+      end
+
+      it 'should fail a have_many :fuus expectation' do
+        expect {
+          expect(subject).to have_many(:fuus)
+        }.to fail_with("expected #{ subject } to define a 'has_many :fuus' association")
+      end
+
+      it 'should fail a have_one :foos expectation' do
+        expect {
+          expect(subject).to have_one(:foos)
+        }.to fail_with("expected #{ subject } to define a 'has_one :foos' association")
+      end
     end
 
-    it 'should fail a have_many :fuus expectation' do
-      expect {
-        expect(subject).to have_many(:fuus)
-      }.to fail_with("expected #{ subject } to define a 'has_many :fuus' association")
-    end
-
-    it 'should fail a have_one :foos expectation' do
-      expect {
-        expect(subject).to have_one(:foos)
-      }.to fail_with("expected #{ subject } to define a 'has_one :foos' association")
+    describe 'key expectation' do
+      it 'should fail a have_many :foos as :bars expectation' do
+        expect {
+          expect(subject).to have_many(:foos).as(:bars)
+        }.to fail_with("expected #{ subject } 'has_many :foos' association to explicitly have key :bars but instead has none")
+      end
     end
   end
 end
