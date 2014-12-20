b24e5315fab3abd2aa8e6944cfce12a2052af578
Tony Ta <tonyta.tt@gmail.com>
2014-12-19 19:52:57 -0800

    removes unneccessary dependencies

--- lib/active_model_serializers_matchers.rb
+++ lib/active_model_serializers_matchers.rb
@@ -1,14 +1,7 @@
-require "active_support"
-
 require "active_model_serializers_matchers/version"
 require "active_model_serializers_matchers/association_matcher"
 
 module ActiveModelSerializersMatchers
-  extend ActiveSupport::Concern
-
-  included do
-    subject { described_class }
-  end
 
   def have_many(association_root)
     AssociationMatcher.new(root: association_root, type: :has_many)
