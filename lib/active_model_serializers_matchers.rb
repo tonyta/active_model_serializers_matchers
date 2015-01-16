1df1c40635dbdd1d8d9ec3ebadb28168e65b9c72
Ben A Morgan <ben@benmorgan.io>
2015-01-15 19:07:36 -0500

    set subject to described class

--- lib/active_model_serializers_matchers.rb
+++ lib/active_model_serializers_matchers.rb
@@ -1,6 +1,10 @@
 Dir[Pathname(__FILE__).join('../**/*.rb')].each { |f| require f }
 
 module ActiveModelSerializersMatchers
+  def self.included(base)
+    base.subject { described_class }
+  end
+
   def have_many(association_root)
     AssociationMatcher.new(association_root, :has_many)
   end
